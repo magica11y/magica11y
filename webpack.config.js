7870a3822ba7b1fa30463f125ed6e3c879cb20c6
Rishabh Rao <rishabhsrao@gmail.com>
2019-06-01 00:19:29 +0200

    🔧 Generate sourcemaps

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require('path');
 
+const TerserPlugin = require('terser-webpack-plugin');
+
 module.exports = {
   entry: {
     prefersReduceMotion: './src/prefersReduceMotion',
@@ -8,6 +10,14 @@ module.exports = {
     filename: '[name].min.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  optimization: {
+    minimizer: [
+      new TerserPlugin({
+        sourceMap: true,
+      }),
+    ],
+  },
+  devtool: 'source-map',
   module: {
     rules: [
       {
